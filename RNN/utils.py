dbbc556ffb9f0ab3df8f21ecbb9d9fbefba370f1
Stephen Wright <62689098+StphnWright@users.noreply.github.com>
2022-05-08 15:22:48 -0400

    Update utils.py

--- RNN/utils.py
+++ RNN/utils.py
@@ -1,10 +1,3 @@
-"""
-COMS 4705 Natural Language Processing Spring 2022 Zhou Yu
-Adapted from COMS 4705 Natural Language Processing Spring 2021 Kathy McKeown
-Homework 3: Sentiment Classification with Naive Bayes - Utils File
-"""
-
-# Imports
 import nltk
 import numpy as np
 import pandas as pd
